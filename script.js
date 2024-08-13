16b0a693628e24067c2b67654ac046f93defe92f
abhishek-475 <61498092+abhishek-475@users.noreply.github.com>
2024-08-13 12:06:20 +0530

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-
 let currentQuery = "sports";
 let currentPage = 1;
-let str = "";
 const fetchNews = async (page, q) => {
+  let str = "";
+
   console.log(`Fetching news for${q}, Page number ${page}... `);
 
   var url =
@@ -42,14 +41,11 @@ const fetchNews = async (page, q) => {
   //       </div>`;
 
   // });
- 
+
   for (let item of response.articles) {
     resultCount.innerHTML = response.totalResults;
-    str +=
-      ` <div class="card my-4 mx-2" style="width: 18rem">
-          <img src="${
-            item.urlToImage
-          }" class="card-img-t
+    str += ` <div class="card my-4 mx-2" style="width: 18rem">
+          <img src="${item.urlToImage}" class="card-img-t
           op" alt="..." height="184" />
           <div class="card-body">
             <h5 class="card-title">${item.title.slice(0, 30)}...</h5>
@@ -61,26 +57,45 @@ const fetchNews = async (page, q) => {
           </div>
         </div>`;
   }
+  // console.log(str);
+
   document.querySelector(".content").innerHTML = str;
 };
 fetchNews(1, currentQuery);
-search.addEventListener("click", (e) => {
-  e.preventDefault();
+
+function searchValue(e) {
   let query = searchInput.value;
   currentQuery = query;
   fetchNews(1, query);
-});
-prev.addEventListener("click", (e) => {
-  e.preventDefault();
+}
+function prevPage() {
   if (currentPage > 1) {
     currentPage = currentPage - 1;
-    // currentQuery = query;
     fetchNews(currentPage, currentQuery);
   }
-});
-next.addEventListener("click", (e) => {
-  e.preventDefault();
+}
+function nextPage() {
   currentPage = currentPage + 1;
-  // currentQuery = query;
   fetchNews(currentPage, currentQuery);
-});
+}
+
+// search.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let query = searchInput.value;
+//   currentQuery = query;
+//   fetchNews(1, query);
+// });
+// prev.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (currentPage > 1) {
+//     currentPage = currentPage - 1;
+//     // currentQuery = query;
+//     fetchNews(currentPage, currentQuery);
+//   }
+// });
+// next.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   currentPage = currentPage + 1;
+//   // currentQuery = query;
+//   fetchNews(currentPage, currentQuery);
+// });
